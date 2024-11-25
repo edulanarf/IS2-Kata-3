3b481472093469f3111967341ed4b158ae0329eb
edulanarf <edulanarfrgmail.com>
2024-11-25 19:53:15 +0000

    Modifico la clase main para mostrar el histograma correctamente. Programa terminado

--- src/main/java/ulpgc/es/Main.java
+++ src/main/java/ulpgc/es/Main.java
@@ -1,26 +1,22 @@
 package ulpgc.es;
 
 import ulpgc.es.control.TitleReader;
+import ulpgc.es.control.TitleTypeHistogram;
 import ulpgc.es.control.TsvTitleReader;
-import ulpgc.es.model.HistogramGenerator;
-import ulpgc.es.model.Title;
-
+import ulpgc.es.model.Histogram;
+import ulpgc.es.view.MainFrame;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
-        TitleReader reader = new TsvTitleReader(new File("./title.basics.tsv"), true);
-        generateHistogram(reader);
-    }
-
-    private static void generateHistogram(TitleReader reader) throws IOException {
-        List<Title> titles = reader.read();
-        HistogramGenerator generator = new HistogramGenerator();
-        Map<Title.TitleType, Integer> histogram = generator.generate(titles);
+        TitleReader reader = new TsvTitleReader(new File("./title.basics.tsv"));
+        Histogram histogram = new TitleTypeHistogram(reader.read());
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.displayhistogram(histogram);
+        mainFrame.setVisible(true);
     }
 }
